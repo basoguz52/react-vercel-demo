c3455199a985dc1395397258890cfe48a1f94d5a
Oğuzhan Baş <oguzhan.bas@bil.omu.edu.tr>
2025-05-08 00:15:27 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Merhaba Vercel!</h1>
+      <p>Bu site React ile yapıldı ve Vercel'de yayınlandı 🚀</p>
     </div>
   );
 }
